d997dbe2d20f87889e6271483a34954e9ce3ad4d
Hossanadev <hossanacc@gmail.com>
2024-07-15 07:31:57 +0100

    login - form validation

--- src/main/java/com/iReserve/controller/Auth.java
+++ src/main/java/com/iReserve/controller/Auth.java
@@ -1,17 +1,33 @@
 package com.iReserve.controller;
 
+import com.iReserve.dto.UserRequest;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class Auth {
     @GetMapping("sign-up")
-    public String signUp() {
+    public String signUp(Model model) {
+        model.addAttribute("userRequest", new UserRequest());
         return "auth/sign-up";
     }
 
     @GetMapping("/login")
     public String login() {
+        return "auth/login";
+    }
+
+    @PostMapping("/auth/sign-up")
+    public String signUp(@Valid @ModelAttribute UserRequest userRequest, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("userRequest", userRequest);
+            return "auth/sign-up";
+        }
         return "auth/login";
     }
 }
--- src/main/java/com/iReserve/dto/UserRequest.java
+++ src/main/java/com/iReserve/dto/UserRequest.java
@@ -1,9 +1,15 @@
 package com.iReserve.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UserRequest {
+    @NotEmpty(message = "Username is required!")
     private String username;
+
+    @Length(min = 8, message = "Password must be at least 8 characters!")
+    @NotEmpty(message = "Password is required!")
     private String password;
 }
